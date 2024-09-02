e9f6a068031c3e8122d9f7973e4c0a7c09faa480
SalavessaMota <nunosalavessa@hotmail.com>
2024-09-02 16:51:24 +0100

    Add project files.

---
+++ AirCinelMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AirCinelMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
